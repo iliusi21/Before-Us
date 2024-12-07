01c115b01522e2cf3795ca22a1d5e52d7dd29501
iliusi <iliusihoyos21@gmail.com>
2024-12-07 00:19:52 -0600

    w

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "code",          // Carpeta de entrada con tus archivos .njk
       output: "docs",         // Carpeta de salida para los archivos generados
-      
+       includes: "_includes"
     },
   };
 };
